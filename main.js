3411d25e88044cffd9086b01ef679e3df74348cd
Roy Dong <roy.dongwei@gmail.com>
2012-03-23 11:58:10 +0800

    a

--- main.js
+++ main.js
@@ -6,60 +6,39 @@ var div = document.createElement( 'div' );
 div.className = 'canvas-box';
 var scene = new Scene;
 var camera = new Camera( 0 , 0 , 920 , 620 , 3 , document.getElementById( 'canvas-frame' ) );
-var tanks = [];
-var actions = [];
-
 var span = document.getElementById( 'fps-show' );
-
-
-Frame.loop = function(){
-
-    span.textContent = this.fps + 'fps' ;
-
-    var mouse = camera.getMouse();
-
-    tank1.lookAt( mouse.x , mouse.y );
-
-    if( mouse.state === Camera.MOUSE_STATE_DOWN ){
-        tank.lookAt( mouse.x , mouse.y );
-    }
-
-
-    //scene.add( tank );
-    scene.add( tank1 );
-    scene.add( tank1.bomb );
-    scene.perform();
-    camera.shoot( scene );
-};
+var bombs = [];
 
 images.on( 'load', function( name ){
     console.log( name  );
 });
 
 images.on( 'complete', function(){
-    tank = new Animation( images.get( 5 ) , 50  , 50  , 1 );
-    move = new Move( tank , 0.3 );
-    f = new Forward( tank , 0.4 );
 
-    tank1 = new Tank( images.get( 5 ) );
-    m = new Forward( tank1 , 0.2 );
-
-    camera.on( 'mousedown' , function(){
-        var mouse = camera.getMouse();
-        f.start();
-    });
+    chamber = new Chamber( Bomb , images.get( 6 ) , 1.2 , 500 , 100 );
+    chamber.position = new Point( 200 , 200 , 1 );
+    chamber.rotation = new Rotation;
 
     camera.on( 'click' , function(){
-        tank1.fire();
-    });
+        var mouse = camera.getMouse();
 
-    camera.on( 'mouseup' , function(){
-        var mouse = this.getMouse();
-        f.stop();
-    });
 
+        chamber.start();
+    });
+    
     Frame.start();
 });
+
+Frame.onloop = function(){
+    span.textContent = this.fps;
+
+    for( var i = 0; i < bombs.length; i++ ){
+        bombs[i].attach( scene );
+    }
+
+    scene.perform();
+    camera.shoot( scene );
+};
 
 function test(){
     for( i = 0; i < 200000; i++){ 
