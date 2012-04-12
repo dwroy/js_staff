d6bbbe2a211124db3f5a2668f6dfbc6e754d8cd4
Roy Dong <roy.dongwei@gmail.com>
2012-04-12 18:08:40 +0800

    geometry

--- main.js
+++ main.js
@@ -3,99 +3,109 @@
  */
 window.on( 'load' , function(){
 
-    images.on( 'load', function( name ){
-        console.log( name  );
-    });
+    images.on( 'load' , function( name ){
+        console.log( name );
+    } );
 
-    images.on( 'complete', function(){
-        var scene = new Scene;
-        var camera = new Camera( 0 , 0 , 960 , 640 , 3 );
+    images.on( 'complete' ,
+            function(){
+                var scene = new Scene;
+                var camera = new Camera( 0 , 0 , 960 , 640 , 3 );
 
-        var npc = [
-            new Npc( new Animation( images.get( 5 ) , 100 , 150 ) , 3000 ) ,
-            new Npc( new Animation( images.get( 5 ) , 200 , 50  ) , 4000 ) ,
-            new Npc( new Animation( images.get( 5 ) , 300 , 300 ) , 1000 ) ,
-            new Npc( new Animation( images.get( 5 ) , 400 , 100 ) , 2000 ) ,
-            new Npc( new Animation( images.get( 5 ) , 700 , 500 ) , 1000 ) ,
-            new Npc( new Animation( images.get( 5 ) , 800 , 150 ) , 3000 ) ,
-            new Npc( new Animation( images.get( 5 ) , 900 , 50  ) , 4000 ) ,
-            new Npc( new Animation( images.get( 5 ) , 1000 , 300 ) , 1000 ) ,
-            new Npc( new Animation( images.get( 5 ) , 1100 , 100 ) , 2000 ) ,
-            new Npc( new Animation( images.get( 5 ) , 1200 , 500 ) , 1000 ) ,
-            new Npc( new Animation( images.get( 5 ) , 1300 , 150 ) , 3000 ) ,
-            new Npc( new Animation( images.get( 5 ) , 1500 , 50  ) , 4000 ) ,
-            new Npc( new Animation( images.get( 5 ) , 1500 , 300 ) , 1000 ) ,
-            new Npc( new Animation( images.get( 5 ) , 1600 , 100 ) , 2000 ) ,
-            new Npc( new Animation( images.get( 5 ) , 1700 , 500 ) , 1000 ) ,
-        ];
+                var npc = [
+                        new Npc( new Animation( images.get( 5 ) , 100 , 150 ) ,
+                                3000 ) ,
+                        new Npc( new Animation( images.get( 5 ) , 200 , 50 ) ,
+                                4000 ) ,
+                        new Npc( new Animation( images.get( 5 ) , 300 , 300 ) ,
+                                1000 ) ,
+                        new Npc( new Animation( images.get( 5 ) , 400 , 100 ) ,
+                                2000 ) ,
+                        new Npc( new Animation( images.get( 5 ) , 700 , 500 ) ,
+                                1000 ) ,
+                        new Npc( new Animation( images.get( 5 ) , 800 , 150 ) ,
+                                3000 ) ,
+                        new Npc( new Animation( images.get( 5 ) , 900 , 50 ) ,
+                                4000 ) ,
+                        new Npc( new Animation( images.get( 5 ) , 1000 , 300 ) ,
+                                1000 ) ,
+                        new Npc( new Animation( images.get( 5 ) , 1100 , 100 ) ,
+                                2000 ) ,
+                        new Npc( new Animation( images.get( 5 ) , 1200 , 500 ) ,
+                                1000 ) ,
+                        new Npc( new Animation( images.get( 5 ) , 1300 , 150 ) ,
+                                3000 ) ,
+                        new Npc( new Animation( images.get( 5 ) , 1500 , 50 ) ,
+                                4000 ) ,
+                        new Npc( new Animation( images.get( 5 ) , 1500 , 300 ) ,
+                                1000 ) ,
+                        new Npc( new Animation( images.get( 5 ) , 1600 , 100 ) ,
+                                2000 ) ,
+                        new Npc( new Animation( images.get( 5 ) , 1700 , 500 ) ,
+                                1000 ) , ];
 
-        var tank = new Tank( images.get( 5 ) , 0.1 );
-        var cannon = new Cannon( Bomb , images.get( 6 ) , 1.5 , 5000 , 20 , 500  );
+                var tank = new Tank( images.get( 5 ) , 0.1 );
+                var cannon = new Cannon( Bomb , images.get( 6 ) , 1.5 , 5000 ,
+                        20 , 500 );
 
-        tank.mount( cannon );
+                tank.mount( cannon );
 
-        tank.on( 'hit' , function( targets ){
-            var npc;
-            while( ( npc = targets.pop() ) ){
-                if( ( npc = npc.owner ) && npc instanceof Npc ){
-                    npc.die();
-                }
-            }
-        });
+                tank.on( 'hit' , function( targets ){
+                    var npc;
+                    while( ( npc = targets.pop() ) ){
+                        if( ( npc = npc.owner ) && npc instanceof Npc ){
+                            npc.die();
+                        }
+                    }
+                } );
 
-        tank.on( 'intersect' , function(){
-            var location = this.animation.location;
-            location.x -= this.lastDx;
-            location.y -= this.lastDy;
-        });
+                tank.on( 'intersect' , function(){
+                    var location = this.animation.location;
+                    location.x -= this.lastDx;
+                    location.y -= this.lastDy;
+                } );
 
-/**
-        camera.on( 'mousedown' , function(){
-            tank.start();
-        });
+                /**
+                 * camera.on( 'mousedown' , function(){ tank.start(); });
+                 * 
+                 * camera.on( 'mouseup' , function(){ tank.stop(); });
+                 * 
+                 * camera.on( 'keydown' , function(){ tank.cannon.fire(); });
+                 */
 
-        camera.on( 'mouseup' , function(){
-            tank.stop();
-        });
+                camera.addEvent( 'touchstart' , function( e ){
+                    var touches = e.targetTouches;
 
-        camera.on( 'keydown' , function(){
-            tank.cannon.fire();
-        });
-        */
+                    tank.start();
+                    tank.animation.lookAt( touches[ 0 ].pageX - camera.left ,
+                            touches[ 0 ].pageY - camera.top );
+                    tank.cannon.fire();
+                } );
 
-        camera.addEvent( 'touchstart' , function( e ){
-            var touch , 
-                touches = e.targetTouches;
+                camera.addEvent( 'touchend' , function( e ){
+                    tank.stop();
+                } );
 
-            tank.start();
-            tank.animation.lookAt( touches[0].pageX - camera.left , touches[0].pageY - camera.top );
-            tank.cannon.fire();
-        });
+                Frame.onloop = function(){
+                    var mouse = camera.getMouse();
 
-        camera.addEvent( 'touchend' , function( e ){
-            tank.stop();  
-        });
-        
-        Frame.onloop = function(){
-            var mouse = camera.getMouse();
+                    if( mouse.state === Camera.MOUSE_STATE_DOWN
+                            && tank.state === Fx.STATE_RUN ){
+                        tank.animation.lookAt( mouse.x , mouse.y );
+                    }
 
-            if( mouse.state === Camera.MOUSE_STATE_DOWN && tank.state === Fx.STATE_RUN ){
-                tank.animation.lookAt( mouse.x , mouse.y );
-            }
+                    tank.attach( scene );
 
-            tank.attach( scene );
+                    for( var i = 0 ; i < npc.length ; i++ ){
+                        npc[ i ].attach( scene );
+                    }
 
-            for( var i = 0 ; i < npc.length ; i++ ){
-                npc[i].attach( scene );
-            }
+                    camera.stick( tank.animation.location );
+                    scene.perform();
+                    camera.shoot( scene );
+                };
 
-            camera.stick( tank.animation.location );
-            scene.perform();
-            camera.shoot( scene );
-        };
-
-
-        window.scrollTo( 0 , 0 , 1 );
-        Frame.start();
-    });
-});
+                window.scrollTo( 0 , 0 , 1 );
+                Frame.start();
+            } );
+} );
